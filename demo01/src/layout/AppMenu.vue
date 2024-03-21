3d578745c86e0f833adbceccf7d89344386dec4b
QixuanCao <cao2573943723@163.com>
2024-03-21 18:14:35 +0800

    demo main

--- demo01/src/layout/AppMenu.vue
+++ demo01/src/layout/AppMenu.vue
@@ -20,11 +20,42 @@
     <el-sub-menu index="2">
       <template #title>
         <el-icon>
-
+          <Menu/>
         </el-icon>
+        <span>数据导入</span>
       </template>
-
+      <el-menu-item index="2-1">文本采集</el-menu-item>
+      <el-menu-item index="2-2">结构化数据</el-menu-item>
+      <el-menu-item index="2-2">管理员数据导入</el-menu-item>
     </el-sub-menu>
+    <el-sub-menu index="3">
+      <template #title>
+        <el-icon>
+          <Grid/>
+        </el-icon>
+        <span>数据分析</span>
+      </template>
+      <el-menu-item index="3-1">知识图谱</el-menu-item>
+      <el-menu-item index="3-2">数据统计</el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="4">
+      <el-icon>
+        <Grid/>
+      </el-icon>
+      <template #title>故障诊断</template>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <el-icon>
+        <Histogram/>
+      </el-icon>
+      <template #title>故障预测</template>
+    </el-menu-item>
+    <el-menu-item index="6">
+      <el-icon>
+        <Promotion/>
+      </el-icon>
+      <template #title>案例分析</template>
+    </el-menu-item>
   </el-menu>
 </template>
 <script lang="ts">
@@ -38,7 +69,7 @@ import {
 
 export default {
   name: "AppMenu",
-  props: [],
+  props: ["isCollapse"],
   emits: [],
   components: [],
   data() {
